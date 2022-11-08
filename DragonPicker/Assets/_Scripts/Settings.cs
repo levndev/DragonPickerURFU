c2209ec6b9ac6b8f752623c9b54d43f835c61e39
Lev Nazarov <levndev@gmail.com>
2022-11-08 18:15:25 +0500

    Фикс слайдеров

--- DragonPicker/Assets/_Scripts/Settings.cs
+++ DragonPicker/Assets/_Scripts/Settings.cs
@@ -14,13 +14,31 @@ public class Settings : MonoBehaviour
     public void MasterVolumeChanged()
     {
         audioMixer.SetFloat("MasterVolume", Mathf.Log10(MasterSlider.value) * 20);
+        PlayerPrefs.SetFloat("MasterVolume", MasterSlider.value);
     }
     public void SFXVolumeChanged()
     {
         audioMixer.SetFloat("SFXVolume", Mathf.Log10(SFXSlider.value) * 20);
+        PlayerPrefs.SetFloat("SFXVolume", SFXSlider.value);
     }
     public void MusicVolumeChanged()
     {
         audioMixer.SetFloat("MusicVolume", Mathf.Log10(MusicSlider.value) * 20);
+        PlayerPrefs.SetFloat("MusicVolume", MusicSlider.value);
+    }
+
+    private void OnEnable()
+    {
+        if (PlayerPrefs.HasKey("MasterVolume"))
+            MasterSlider.value = PlayerPrefs.GetFloat("MasterVolume");
+        if (PlayerPrefs.HasKey("SFXVolume"))
+            SFXSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+        if (PlayerPrefs.HasKey("MusicVolume"))
+            MusicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+    }
+
+    private void OnDisable()
+    {
+        PlayerPrefs.Save();
     }
 }
